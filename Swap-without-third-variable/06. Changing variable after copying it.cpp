0bfc5aaa13b6b857cfebebc150f0bf134fe0bffd
rosque <helps2001@gmail.com>
2019-10-13 17:33:48 -0300

    Update 06. Changing variable after copying it.cpp

--- Swap-without-third-variable/06. Changing variable after copying it.cpp
+++ Swap-without-third-variable/06. Changing variable after copying it.cpp
@@ -2,12 +2,19 @@
 
 using namespace std;
 
+int swap(int &a, int&b) {
+  b = a;
+  return 0;
+}
+
+
+
 int main() {
     int a = 5;
     int b = 50;
 
 
-    a =  b + (b = a , 0);
+    a =  b + swap(a,b);
 
     cout << a << " " << b << endl;
 
